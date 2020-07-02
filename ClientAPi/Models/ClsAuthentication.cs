61aa72c31f24f140a5d8f33615f4a550e9ca03d6
mchoc <mynorrene@choccac.com>
2020-07-02 14:21:12 -0600

    Star Proyect API Sever C# MVC API .Net 4.7

---
+++ ClientAPi/Models/ClsAuthentication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClientAPi.Models
+{
+    public class ClsAuthentication
+    {
+        public string key { get; set; }
+        public string shared_secret { get; set; }
+
+
+    }
+}
